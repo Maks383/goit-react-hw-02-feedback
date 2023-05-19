5fec943212e0763f7b270f84809df176c1756b19
Maks383 <m.fitjo10@gmail.com>
2023-05-19 16:25:17 +0300

    add

---
+++ src/components/statistics/StatisticsList.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StatisticsList = styled.ul`
+  list-style: none;
+`;
+
+export const StatisticsItem = styled.li`
+  display: flex;
+  gap: 10px;
+  font-size: 18px;
+`;
